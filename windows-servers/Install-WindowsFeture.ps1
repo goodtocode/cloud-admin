8e54ccb6380e70d7da6d3c9998d5ff27ef7d6562
Robert Good <robert.good@aacn.org>
2023-10-12 15:19:55 -0700

    Get-WindowsCapability

---
+++ windows-servers/Install-WindowsFeture.ps1
@@ -0,0 +1,30 @@
+####################################################################################
+# To execute
+#   1. Run Powershell as ADMINistrator
+#   2. In powershell, set security polilcy for this script: 
+#      Set-ExecutionPolicy Unrestricted -Scope Process -Force
+#   3. Change directory to the script folder:
+#      CD C:\Temp (wherever your script is)
+#   4. In powershell, run script: 
+#      .\Install-WindowsFeture.ps1 -Feature RSAT*
+####################################################################################
+
+param (
+	[Parameter(Mandatory=$true,ValueFromPipelineByPropertyName=$true)]
+ 	[string]$Feature = $(throw '-Feature is a required parameter.') #RSAT
+)
+
+####################################################################################
+Set-ExecutionPolicy Unrestricted -Scope Process -Force
+$VerbosePreference = 'SilentlyContinue' # 'SilentlyContinue' # 'Continue'
+[String]$ThisScript = $MyInvocation.MyCommand.Path
+[String]$ThisDir = Split-Path $ThisScript
+Set-Location $ThisDir # Ensure our location is correct, so we can use relative paths
+Write-Host "*****************************"
+Write-Host "*** Starting: $ThisScript On: $(Get-Date)"
+Write-Host "*****************************"
+####################################################################################
+# Imports
+Import-Module "./System.psm1"
+
+Get-WindowsCapability -Name RSAT* -Online
