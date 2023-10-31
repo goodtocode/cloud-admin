bb16673c99bedd25a7d55370a8fdd86bb43d3683
Robert Good <robert.good@aacn.org>
2023-10-31 13:47:40 -0700

    Restart-WebAppPool

---
+++ iis-web-server/Restart-WebAppPool.ps1
@@ -0,0 +1,31 @@
+####################################################################################
+# To execute
+#   1. Run Powershell as ADMINistrator
+#   2. In powershell, set security polilcy for this script: 
+#      Set-ExecutionPolicy Unrestricted -Scope Process -Force
+#   3. Change directory to the script folder:
+#      CD C:\Temp (wherever your script is)
+#   4. In powershell, run script: 
+#      .\Restart-WebAppPool.ps1 -Name "www.mysite.com"
+####################################################################################
+
+param (
+	[Parameter(Mandatory=$true,ValueFromPipelineByPropertyName=$true)]
+ 	[string]$Name = $(throw '-Name is a required parameter.') #www.mysite.com
+)
+
+####################################################################################
+Set-ExecutionPolicy Unrestricted -Scope Process -Force
+$VerbosePreference = 'SilentlyContinue' # 'SilentlyContinue' # 'Continue'
+[String]$ThisScript = $MyInvocation.MyCommand.Path
+[String]$ThisDir = Split-Path $ThisScript
+Set-Location $ThisDir # Ensure our location is correct, so we can use relative paths
+Write-Host "*****************************"
+Write-Host "*** Starting: $ThisScript On: $(Get-Date)"
+Write-Host "*****************************"
+####################################################################################
+# Modules
+Import-Module "./System.psm1"
+Install-Module -Name IISAdministration
+
+Restart-WebAppPool -Name $Name
