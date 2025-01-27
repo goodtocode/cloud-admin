af25d243e17b96318d32701ca7a29c3c7feaa426
Robert Good <robert.good@aacn.org>
2025-01-27 10:38:49 -0800

    scipt tested

--- scripts/azure-resources/storage-account/Copy-StorageAccountContainer.ps1
+++ scripts/azure-resources/storage-account/Copy-StorageAccountContainer.ps1
@@ -10,12 +10,14 @@
 ####################################################################################
 
 param (
-    [Parameter(Mandatory=$true)][string]$SrcResourceGroupName,
-    [Parameter(Mandatory=$true)][string]$SrcStorageAccountName,
-    [Parameter(Mandatory=$true)][string]$SrcContainer,
-    [Parameter(Mandatory=$true)][string]$DestResourceGroupName,
-    [Parameter(Mandatory=$true)][string]$DestStorageAccountName,
-    [Parameter(Mandatory=$true)][string]$DestContainer
+    [string]$TenantId,
+    [string]$SubscriptionId,
+    [string]$SrcResourceGroupName,
+    [string]$SrcStorageAccountName,
+    [string]$SrcContainer,
+    [string]$DestResourceGroupName,
+    [string]$DestStorageAccountName,
+    [string]$DestContainer
 )
 
 ####################################################################################
@@ -23,14 +25,30 @@ Set-ExecutionPolicy Unrestricted -Scope Process -Force
 $VerbosePreference = 'SilentlyContinue' # 'SilentlyContinue' # 'Continue'
 ####################################################################################
 
+# Imports
+if (-not (Get-Module -ListAvailable -Name Az.Accounts)) { Install-Module -Name Az.Accounts -AllowClobber -Scope CurrentUser -Force }
+if (-not (Get-Module -Name Az.Accounts)) { Import-Module -Name Az.Accounts -Force}
+if (-not (Get-Module -ListAvailable -Name Az.Resources)) { Install-Module -Name Az.Resources -AllowClobber -Scope CurrentUser -Force }
+if (-not (Get-Module -Name Az.Resources)) { Import-Module -Name Az.Resources -Force}
+if (-not (Get-Module -ListAvailable -Name Az.Storage)) { Install-Module -Name Az.Storage -AllowClobber -Scope CurrentUser -Force }
+if (-not (Get-Module -Name Az.Storage)) { Import-Module -Name Az.Storage -Force}
+
+# Auth
+Connect-AzAccount -Tenant $TenantId -Subscription $SubscriptionId
+
+# Execute
 $SrcStorageKey = (Get-AzStorageAccountKey -ResourceGroupName $SrcResourceGroupName -Name $SrcStorageAccountName)[0].Value
 $DestStorageKey = (Get-AzStorageAccountKey -ResourceGroupName $DestResourceGroupName -Name $DestStorageAccountName)[0].Value
 
 $SrcContext = New-AzStorageContext -StorageAccountName $SrcStorageAccountName -StorageAccountKey $SrcStorageKey
 $DestContext = New-AzStorageContext -StorageAccountName $DestStorageAccountName -StorageAccountKey $DestStorageKey
 
-Get-AzStorageBlob -Container $SrcContainer -Context $SrcContext | ForEach-Object {
-    Start-AzStorageBlobCopy -SrcBlob $_.Name -DestContainer $DestContainer -DestContext $DestContext -Force
+# Check if destination container exists, if not, create it
+$DestContainerExists = Get-AzStorageContainer -Context $DestContext -Name $DestContainer -ErrorAction SilentlyContinue
+if (-not $DestContainerExists) {
+    New-AzStorageContainer -Name $DestContainer -Context $DestContext
 }
 
-
+Get-AzStorageBlob -Container $SrcContainer -Context $SrcContext | ForEach-Object {
+    Start-AzStorageBlobCopy -SrcContainer $SrcContainer -SrcBlob $_.Name -SrcContext $SrcContext -DestContainer $DestContainer -DestContext $DestContext -Force
+}
