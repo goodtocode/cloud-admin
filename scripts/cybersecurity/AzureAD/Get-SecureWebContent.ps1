55d4a7dc991406e9e90789ed163586e3dbcd7f7c
Robert Good <robert.good@aacn.org>
2025-02-19 15:27:20 -0800

    New ps1 for testing secured get request

--- scripts/cybersecurity/AzureAD/Get-SecureWebContent.ps1
+++ scripts/cybersecurity/AzureAD/Get-SecureWebContent.ps1
@@ -6,14 +6,15 @@
 #   3. Change directory to the script folder:
 #      CD C:\Scripts (wherever your script is)
 #   4. In powershell, run script: 
-#      .\Get-AuthorizationToken.ps1 -TenantId "00000000-85ea-42d2-b7b3-30ef2666c7ab" -ClientId "00000000-9f24-40c0-87b8-69bdd5ae60a3" -ClientSecret "SECRET HERE" -Scope "00000000-4ae8-43f7-8c1f-71b3a340d4fd"
+#      .\Get-SecureWebContent.ps1 -TenantId "00000000-85ea-42d2-b7b3-30ef2666c7ab" -ClientId "00000000-9f24-40c0-87b8-69bdd5ae60a3" -ClientSecret "SECRET HERE" -Scope "00000000-4ae8-43f7-8c1f-71b3a340d4fd"
 ####################################################################################
 
 param (
     [string]$TenantId = $(throw '-TenantId is a required parameter.'),
     [string]$ClientId = $(throw '-ClientId is a required parameter.'),
     [string]$ClientSecret = $(throw '-ClientSecret is a required parameter.'),
-    [string]$ScopeClientId = $(throw '-ScopeClientId is a required parameter.')
+    [string]$ScopeClientId = $(throw '-ScopeClientId is a required parameter.'),
+    [string]$Url = $(throw '-Url is a required parameter.')
 )
 
 ####################################################################################
@@ -27,7 +28,7 @@ Write-Host "*** Starting: $ThisScript On: $(Get-Date)"
 Write-Host "*****************************"
 ####################################################################################
 
-Invoke-RestMethod -Uri "https://login.microsoftonline.com/$TenantId/oauth2/v2.0/token" `
+$tokenResponse = Invoke-RestMethod -Uri "https://login.microsoftonline.com/$TenantId/oauth2/v2.0/token" `
     -Method Post `
     -ContentType "application/x-www-form-urlencoded" `
     -Headers @{"Cookie" = "stsservicecookie=estsfd; x-ms-gateway-slice=estsfd" } `
@@ -37,3 +38,16 @@ Invoke-RestMethod -Uri "https://login.microsoftonline.com/$TenantId/oauth2/v2.0/
     client_secret = $ClientSecret
     scope         = "api://$ScopeClientId/.default"
 }
+
+$accessToken = $tokenResponse.access_token
+Write-Host "Access Token: $accessToken"
+
+$response = Invoke-WebRequest -Uri "$Url" `
+    -Method Get `
+    -Headers @{ Authorization = "Bearer $accessToken" }
+
+$statusCode = $response.StatusCode
+$responseContent = $response.Content
+
+Write-Host "HTTP Status Code: $statusCode"
+Write-Host "Response Content: $responseContent"
