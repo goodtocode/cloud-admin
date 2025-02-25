92bfa190c94e5ecb990a6d67d86e3f8ffe1a2b14
Robert Good <robert.good@aacn.org>
2025-02-25 14:18:23 -0800

    validated working

--- scripts/azure-resources/storage-account/Remove-RecoveryServiceVault.ps1
+++ scripts/azure-resources/storage-account/Remove-RecoveryServiceVault.ps1
@@ -3,9 +3,9 @@
 #
 # Example: .\Remove-RecoveryServiceVault -TenantId -SubscriptionId -ResourceGroup -StorageAccount
 #-----------------------------------------------------------------------
-# ***
-# *** Parameters
-# ***
+# ---
+# --- Parameters
+# ---
 param
 (
 	[string] $TenantId=$(throw '-TenantId is a required parameter. (00000000-0000-0000-0000-000000000000)'),
@@ -13,60 +13,67 @@ param
 	[string] $ResourceGroup=$(throw '-ResourceGroup is a required parameter. (rg-PRODUCT-ENVIRONMENT-001)'),
     [string] $ContainerName=$(throw '-ContainerName is a required parameter. (rg-PRODUCT-ENVIRONMENT-001)'),
     [string] $RecoveryVaultName=$(throw '-RecoveryVaultName is a required parameter. (vaultPRODUCTENVIRONMENT001)'),
-    [string] $ContainerType="AzureStorage",
-    [string] $WorkloadType="AzureFiles"
+    [string] $ContainerType="AzureStorage"
 )
 
-# ***
-# *** Initialize
-# ***
+# ---
+# --- Initialize
+# ---
 if ($IsWindows) { Set-ExecutionPolicy Unrestricted -Scope Process -Force }
 $VerbosePreference = 'SilentlyContinue' #'Continue'
 [String]$ThisScript = $MyInvocation.MyCommand.Path
 [String]$ThisDir = Split-Path $ThisScript
 [DateTime]$Now = Get-Date
 Set-Location $ThisDir # Ensure our location is correct, so we can use relative paths
-Write-Host "*****************************"
-Write-Host "*** Starting: $ThisScript on $Now"
-Write-Host "*****************************"
+Write-Host "---------------------------"
+Write-Host "--- Starting: $ThisScript on $Now"
+Write-Host "---------------------------"
 # Imports
 Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
 Import-Module "../../Azure.psm1"
 Import-Module "../../System.psm1"
-# Install the necessary modules
+# ---
+# --- Install required modules
+# ---
 Install-Module -Name Az.Accounts -AllowClobber -Scope CurrentUser
 Install-Module -Name Az.Resources -AllowClobber -Scope CurrentUser
 Install-Module -Name Az.Storage -AllowClobber -Scope CurrentUser
 Install-Module -Name Az.RecoveryServices -AllowClobber -Scope CurrentUser
 
-# Connect to the Azure account
+# ---
+# --- Auth
+# ---
 Connect-IfNotAuthenticated -TenantId $TenantId -SubscriptionId $SubscriptionId
 
-# Get the Recovery Services vault
+# ---
+# --- Execute
+# ---
 $vault = Get-AzRecoveryServicesVault -Name $RecoveryVaultName
 Set-AzRecoveryServicesVaultContext -Vault $vault
 
-# Get the backup containers
 $containers = Get-AzRecoveryServicesBackupContainer -VaultId $vault.ID -ContainerType $ContainerType
-$containers | Format-Table -Property Name, ContainerType, HealthStatus, FriendlyName
+foreach ($container in $containers) {
+    $container | Format-Table -Property Name, ContainerType, HealthStatus, FriendlyName
 
-# Get the specific container
-$container = $containers | Where-Object { $_.FriendlyName -eq $ContainerName }
+    switch ($container.ContainerType) {
+        "AzureVM" { $WorkloadType = "AzureVM" }
+        "MAB" { $WorkloadType = "MAB" }
+        "AzureSQL" { $WorkloadType = "SQLDatabase" }
+        default { $WorkloadType = "AzureFiles" }
+    }
 
-$softDeletedItems = Get-AzRecoveryServicesBackupItem -VaultId $vault.ID -Container $container -WorkloadType $WorkloadType | Where-Object { $_.DeleteState -eq "ToBeDeleted" }
-foreach ($item in $softDeletedItems) {
-    Write-host "Undo ByContainer: $($item.Name) from $($item.ContainerName)"
-    $item | Format-Table -Property Name, ContainerName, WorkloadType, ProtectionStatus, DeleteState
-    Undo-AzRecoveryServicesBackupItemDeletion -Item $item -VaultId $vault.ID
-    Disable-AzRecoveryServicesBackupProtection -Item $item -RemoveRecoveryPoints -Force
+    $backupItems = Get-AzRecoveryServicesBackupItem -VaultId $vault.ID -Container $container -WorkloadType $WorkloadType
+    foreach ($item in $backupItems) {
+        $item | Format-Table -Property Name, ContainerName, WorkloadType, ProtectionStatus, DeleteState
+        if ($item.WorkloadType -eq 'AzureVM') {
+            Write-host "Undo-AzRecoveryServicesBackupItemDeletion -Item $item -VaultId $vault.ID"
+            Undo-AzRecoveryServicesBackupItemDeletion -Item $item -VaultId $vault.ID
+        } else {
+            Write-Host "Undo-deletion is not supported for $($item.WorkloadType)"
+        }
+        Write-host "Disable-AzRecoveryServicesBackupProtection -Item $item -RemoveRecoveryPoints -Force"    
+        Disable-AzRecoveryServicesBackupProtection -Item $item -RemoveRecoveryPoints -Force
+    }
 }
 
-#$backupItems = Get-AzRecoveryServicesBackupItem -VaultId $vault.ID -BackupManagementType $ContainerType -WorkloadType $WorkloadType
-$backupItems = Get-AzRecoveryServicesBackupItem -VaultId $vault.ID -Container $container -WorkloadType $WorkloadType
-foreach ($item in $backupItems) {
-    Write-host "Disable ByContainer: $($item.Name) from $($item.ContainerName)"
-    $item | Format-Table -Property Name, ContainerName, WorkloadType, ProtectionStatus, DeleteState
-    Disable-AzRecoveryServicesBackupProtection -Item $item -RemoveRecoveryPoints -Force
-}
-
-Remove-AzRecoveryServicesVault -Vault $vault -Force
+Remove-AzRecoveryServicesVault -Vault $vault
