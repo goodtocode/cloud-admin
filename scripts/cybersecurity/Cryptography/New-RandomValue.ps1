a2fdc916c75beee1d11fdc7b66c74920a2a0bea8
Robert Good <robert.good@aacn.org>
2025-06-19 14:37:15 -0700

    new random number generator

---
+++ scripts/cybersecurity/Cryptography/New-RandomValue.ps1
@@ -0,0 +1,37 @@
+# To execute
+#   1. Run Powershell as ADMINistrator
+#   2. In powershell, set security polilcy for this script: 
+#      Set-ExecutionPolicy Unrestricted -Scope Process -Force
+#   3. Change directory to the script folder:
+#      CD C:\Scripts (wherever your script is)
+#   4. In powershell, run script: 
+#      .\Export-Certificate -Path .\dev.myorg.com.pfx -Password MyPass1234 -Dns dev.myorg.com
+
+param (
+	[int]$Length = 16
+)
+####################################################################################
+Set-ExecutionPolicy Unrestricted -Scope Process -Force
+$VerbosePreference = 'SilentlyContinue' # 'SilentlyContinue' # 'Continue'
+if ($MyInvocation.MyCommand.Path) {
+    [String]$ThisScript = $MyInvocation.MyCommand.Path
+} else {
+    [String]$ThisScript = (Get-Location).Path
+}
+[String]$ThisDir = Split-Path $ThisScript
+Set-Location $ThisDir # Ensure our location is correct, so we can use relative paths
+
+Write-Host "*****************************"
+Write-Host "*** Starting: $ThisScript On: $(Get-Date)"
+Write-Host "*****************************"
+####################################################################################
+
+function New-RandomByteArray {
+    param([int]$length = 20)
+    $chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()-_=+[]{}'
+    $bytes = New-Object byte[] $length
+    [System.Security.Cryptography.RNGCryptoServiceProvider]::Create().GetBytes($bytes)
+    -join ($bytes | ForEach-Object { $chars[$_ % $chars.Length] })
+}
+
+New-RandomByteArray -length $Length
